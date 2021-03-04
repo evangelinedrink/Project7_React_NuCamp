0e7f47da2cc1bea6a8bc0656174458f11ba5b9c6
Evangeline Drink <evangelinedrink@gmail.com>
2021-03-03 22:59:15 -0800

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {Navbar, NavbarBrand} from "reactstrap";
+import Directory from "./components/DirectoryComponent";
 import "./App.css";
+
 
 class App extends Component {
   render() {
@@ -11,10 +13,11 @@ class App extends Component {
             <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
+        <Directory />
       </div>
     );
   }
 }
 
-export default App; {/*This means that this function App is the default export for this entire file. JavaScript module is a JavaScript file that must contain one export.*/}
-{/*export {App}; can be used if we don't want to type the above line with default */}
+export default App; //This means that this function App is the default export for this entire file. JavaScript module is a JavaScript file that must contain one export.
+//export {App}; can be used if we don't want to type the above line with default.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; {/*The computer is importing the App function that is located in App.js file. ./ means that the App.js file is in the same folder as this index.js file. 
-In addition, we can use import {App} from "./App" if we make sure to have the export{App}; line in the App.js file.*/}
+import App from './App'; //The computer is importing the App function that is located in App.js file. ./ means that the App.js file is in the same folder as this index.js file. 
+//In addition, we can use import {App} from "./App" if we make sure to have the export{App}; line in the App.js file.*/}
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -17,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
